dddfa708a09d9b68ce1ef518deccc59684baff5d
Brian Kwong <bkwong01@calpoly.edu>
2025-05-04 19:42:15 -0700

    Cleaned up output

--- agent.py
+++ agent.py
@@ -1,45 +1,54 @@
 # %%
+# Helper functions to convert between the board and a column ,row tuple
+
+# %%
 from kaggle_environments import evaluate, make
+
 
 def my_agent(observation, configuration):
     from random import choice
-    print(configuration.columns)
 
-    ##return 0 
+    SELECTED_COLUMN = 1
+
+    ##return 0
     for n in range(configuration.columns):
-        print(n)
-        print(observation.board)
-        print(len(observation.board))
-        print(type(observation.board))
-        print(configuration.columns)
-        print(observation.board[n])
-        print(observation.board[n] == 0)
+        print(
+            f"""
+            *********************************************************
+              Number of columns: {n}
+              Number of rows: {configuration.rows}
+              Total number of cells: {len(observation.board)}
+              Number of Filled cells: {len([c for c in observation.board if c != 0])}
+              -------------------------
+              Board: {observation.board}
+            *********************************************************
+              """
+        )
 
-        
-        return 1
+        return SELECTED_COLUMN
 
+    # Not exactly what this for if 1 is always returned?
+    return choice(
+        [c for c in range(configuration.columns) if observation.board[c] == 0]
+    )
 
-
-    return choice([c for c in range(configuration.columns) if observation.board[c] == 0])
 
 def find_available_boards():
     pass
+
 
 def get_value():
     pass
 
 
-
-
-
 if __name__ == "__main__":
-    env= make("connectx", debug=True)
+    env = make("connectx", debug=True)
     env.render()
 
     env.reset()
     # Play as the first agent against default "random" agent.
     env.run([my_agent, "random"])
-    env.render(mode="ipython", width=500, height=450)   
+    env.render(mode="ipython", width=500, height=450)
 
 
 # %%
