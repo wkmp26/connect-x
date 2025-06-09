251a3cf9416eb472bae4ec697545cec3ffd69440
Mio <manakaga@calpoly.edu>
2025-06-08 17:54:02 -0700

    optimization

--- game_simulation.py
+++ game_simulation.py
@@ -1,3 +1,93 @@
+# from agent_rl import my_agent
+
+# class Configuration():
+#     def __init__(self, columns: int = 7, rows: int = 6):
+#         self.columns = columns
+#         self.rows = rows
+
+# class Observation():
+#     def __init__(self, board: list[int], mark: int):
+#         self.board = board
+#         self.mark = mark
+
+# def find_available_index(board, column):
+
+#     c = column 
+    
+#     if board[c] != 0:
+#         raise Exception("Invalid Move Played")
+
+#     while board[c] == 0:
+#         c = c + 7
+#         if c >= 41:
+#             break   
+#     c = c - 7
+
+#     return c
+
+# def check_win(board):
+#     vertical_points = list(range(21))
+#     horizontal_points = [n + 7*i for i in range(1,6) for n in list(range(4))]
+
+#     #vertical
+#     for p in vertical_points:
+#         points = [p +7*i for i in range(4)]
+#         candidate = [board[i] for i in points]
+#         if candidate == [1,1,1,1]:
+#             return 1
+#         if candidate == [2,2,2,2]:
+#             return 2
+        
+#     #horizontal
+#     for p in horizontal_points:
+#         candidate = board[p:p+4]
+#         if candidate == [1,1,1,1]:
+#             return 1
+#         if candidate == [2,2,2,2]:
+#             return 2
+
+# def print_board(board):
+#     print("------------------------------------")
+#     for r in range(6):
+#         print(" ".join(str(board[r * 7 + c]) for c in range(7)))
+#     print("------------------------------------")
+
+
+# def game_simulation_human(agent):
+#     config = Configuration(7)
+
+#     game_state = Observation([0,0,0,0,0,0,0,
+#                   0,0,0,0,0,0,0,
+#                   0,0,0,0,0,0,0,
+#                   0,0,0,0,0,0,0,
+#                   0,0,0,0,0,0,0,
+#                   0,0,0,0,0,0,0], )
+    
+#     while True:
+#         print("Agent's Turn")
+#         move_1 = agent(observation=game_state,configuration=config)
+#         index_1 = find_available_index(board=game_state.board,column=move_1)
+#         game_state.board[index_1] = 1
+#         print_board(game_state.board)
+
+#         if check_win(game_state.board) == 1:
+#             print("Player One Wins!")
+#             break
+
+#         move_2 = int(input("Enter move (0-6): "))
+#         index_2 = find_available_index(board=game_state.board, column=move_2)
+#         game_state.board[index_2] = 2
+#         print_board(game_state.board)
+
+#         if check_win(game_state.board) == 2:
+#             print("Player Two Wins!")
+#             break
+    
+
+    
+# if __name__ == "__main__":
+#     game_simulation_human(my_agent)
+
 from agent_rl import my_agent
 
 class Configuration():
@@ -6,44 +96,39 @@ class Configuration():
         self.rows = rows
 
 class Observation():
-    def __init__(self, board: list[int]):
+    def __init__(self, board: list[int], mark: int):
         self.board = board
+        self.mark = mark
 
 def find_available_index(board, column):
-
     c = column 
-    
     if board[c] != 0:
         raise Exception("Invalid Move Played")
-
-    while board[c] == 0:
-        c = c + 7
-        if c >= 41:
-            break   
-    c = c - 7
-
+    while c + 7 < len(board) and board[c + 7] == 0:
+        c += 7
     return c
 
 def check_win(board):
     vertical_points = list(range(21))
-    horizontal_points = [n + 7*i for i in range(1,6) for n in list(range(4))]
+    horizontal_points = [n + 7*i for i in range(6) for n in list(range(4))]
 
-    #vertical
+    # Vertical check
     for p in vertical_points:
-        points = [p +7*i for i in range(4)]
+        points = [p + 7*i for i in range(4)]
         candidate = [board[i] for i in points]
         if candidate == [1,1,1,1]:
             return 1
         if candidate == [2,2,2,2]:
             return 2
         
-    #horizontal
+    # Horizontal check
     for p in horizontal_points:
         candidate = board[p:p+4]
         if candidate == [1,1,1,1]:
             return 1
         if candidate == [2,2,2,2]:
             return 2
+    return 0  # no winner
 
 def print_board(board):
     print("------------------------------------")
@@ -51,21 +136,15 @@ def print_board(board):
         print(" ".join(str(board[r * 7 + c]) for c in range(7)))
     print("------------------------------------")
 
-
 def game_simulation_human(agent):
     config = Configuration(7)
 
-    game_state = Observation([0,0,0,0,0,0,0,
-                  0,0,0,0,0,0,0,
-                  0,0,0,0,0,0,0,
-                  0,0,0,0,0,0,0,
-                  0,0,0,0,0,0,0,
-                  0,0,0,0,0,0,0])
+    game_state = Observation([0]*42, mark=1)
     
     while True:
         print("Agent's Turn")
-        move_1 = agent(observation=game_state,configuration=config)
-        index_1 = find_available_index(board=game_state.board,column=move_1)
+        move_1 = agent(observation=game_state, configuration=config)
+        index_1 = find_available_index(board=game_state.board, column=move_1)
         game_state.board[index_1] = 1
         print_board(game_state.board)
 
@@ -73,6 +152,7 @@ def game_simulation_human(agent):
             print("Player One Wins!")
             break
 
+        print("Human's Turn")
         move_2 = int(input("Enter move (0-6): "))
         index_2 = find_available_index(board=game_state.board, column=move_2)
         game_state.board[index_2] = 2
@@ -81,8 +161,6 @@ def game_simulation_human(agent):
         if check_win(game_state.board) == 2:
             print("Player Two Wins!")
             break
-    
 
-    
 if __name__ == "__main__":
     game_simulation_human(my_agent)
